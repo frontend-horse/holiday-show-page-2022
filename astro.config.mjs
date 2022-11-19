68a3c52be56a65757dfea045db12bef78c7d8b8e
Ryan Trimble <mrtrimble2@gmail.com>
2022-11-19 18:59:21 -0500

    fix merge conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(),image()],
 });
