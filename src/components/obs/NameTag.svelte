37e50529361fe589b52fb1392e26b038cb76e149
Alex Trost <19617280+a-trost@users.noreply.github.com>
2022-12-11 09:53:44 -0500

    Get next speaker

--- src/components/obs/NameTag.svelte
+++ src/components/obs/NameTag.svelte
@@ -1,12 +1,22 @@
 <script>
   import { onMount } from 'svelte';
   import { fetchNotionData } from '../../lib/notion';
-  let speakers = fetchNotionData();
+  let speakers;
 
   onMount(async () => {
     speakers = await fetchNotionData();
     console.log('🚀 ~ file: NameTag.svelte:7 ~ speakers', speakers);
+    // fetch notion data every 10 seconds
+    setInterval(async () => {
+      speakers = await fetchNotionData();
+    }, 10000);
   });
+  // Find next item in speakers array with speaker.complete === false
+  $: currentSpeaker = speakers?.find((speaker) => speaker.complete === false);
 </script>
 
-<div>Speaker</div>
+<div>
+  {#if currentSpeaker}
+    {currentSpeaker.name}
+  {/if}
+</div>
