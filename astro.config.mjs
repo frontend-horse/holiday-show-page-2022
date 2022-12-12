0f53db3d8d0d27c9ae7bf2981bda81f56bac0e88
Ben Myers <ben.myers@okstate.edu>
2022-12-12 09:48:43 -0600

    Install  integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.URL || 'http://localhost:3000',
-  integrations: [svelte(), image(), prefetch()]
+  integrations: [svelte(), image(), prefetch(), sitemap()]
 });
